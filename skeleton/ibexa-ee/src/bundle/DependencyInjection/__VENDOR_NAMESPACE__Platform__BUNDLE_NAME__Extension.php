3ccb1d1d040d7cc83117cc25b84e30df503fff48
Adam Wójs <adam@wojs.pl>
2020-07-19 14:25:10 +0200

    Added ibexa-os and ibexa-ee skeletons

---
+++ skeleton/ibexa-ee/src/bundle/DependencyInjection/__VENDOR_NAMESPACE__Platform__BUNDLE_NAME__Extension.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace __VENDOR_NAMESPACE__\Platform\Bundle\__BUNDLE_NAME__\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+final class __VENDOR_NAMESPACE__Platform__BUNDLE_NAME__Extension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../Resources/config')
+        );
+
+        $loader->load('services.yaml');
+    }
+}
---
+++ skeleton/ibexa-ee/src/bundle/__VENDOR_NAMESPACE__Platform__BUNDLE_NAME__Bundle.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace __VENDOR_NAMESPACE__\Platform\Bundle\__BUNDLE_NAME__;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class __VENDOR_NAMESPACE__Platform__BUNDLE_NAME__Bundle extends Bundle
+{
+}
